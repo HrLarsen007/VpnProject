44bd43f1c07184a9c2a51dca891b4e929c316fbf
Hrlarsen007 <hrlarsen007@gmail.com>
2022-10-06 23:41:01 +0200

    ´123

--- nginx_https_docker/reset.sh
+++ nginx_https_docker/reset.sh
@@ -1,1 +1,5 @@
-rm -rf nginx_https_docker/ && rm -rf VpnProject/ && git clone https://github.com/hrlarsen007/VpnProject.git && cp -r VpnProject/nginx_https_docker/ nginx_https_docker && cd nginx_https_docker/
+rm -rf nginx_https_docker/ 
+rm -rf VpnProject/  
+git clone https://github.com/hrlarsen007/VpnProject.git  
+cp -r VpnProject/nginx_https_docker/ nginx_https_docker  
+cd nginx_https_docker/
